a495c0de8fca44f84091c0b0b925cb72c5aa56d5
Skyliuk2k20 <lukas.petryla@triboo.it>
2020-03-26 01:02:45 +0100

    APP: first commit to new branch

---
+++ app/controllers/api/v1/articles_controller.rb
@@ -0,0 +1,47 @@
+module Api
+  module V1
+    class ArticlesController < ApplicationController
+      def index
+        articles = Article.order('created_at DESC');
+        render json: {status: 'SUCCES', message: 'Loaded articles',data:articles}, status: :ok
+      end
+
+      def show
+        article = Article.find(params[:id])
+        render json: {status: 'SUCCES', message: 'Loaded articles',data:article}, status: :ok
+      end
+
+      def create
+        article = Article.new(article_params)
+
+        if article.save
+          render json: {status: 'SUCCES', message: 'Loaded articles' , data:article}, status: :ok
+        else
+          render json: {status: 'error', message: 'articles not saved' , data:article.errors}, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        article = Article.find(params[:id])
+        article.destroy
+        render json: {status: 'SUCCES', message: 'Loaded articles' , data:article}, status: :ok
+      end
+
+      def update
+        article = Article.find(params[:id])
+        if article.update_attributes(article_params)
+          render json: {status: 'SUCCES', message: 'Loaded articles' , data:article}, status: :ok
+        else
+          render json: {status: 'error', message: 'articles not saved' , data:article.errors}, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def article_params
+        params.permit(:title, :body)
+      end
+
+    end
+  end
+end
